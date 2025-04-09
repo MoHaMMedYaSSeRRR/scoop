11063e7e9cd41b2465fa3222030b96bf5a2ab6f6
MoHaMMedYaSSeRRR <mohamed972001yasser@gmail.com>
2025-04-09 13:30:59 +0200

    edits final

--- src/app/services/upload.service.ts
+++ src/app/services/upload.service.ts
@@ -17,12 +17,12 @@ export class UploadService {
   constructor(
     private _HttpClient:HttpClient
   ) { }
-  // upload(data: any): Observable<any> {
-  //   return this._HttpClient.post('https://scoob.cc/step-1', data);
-  // }
   upload(data: any): Observable<any> {
-    return this._HttpClient.post('/scoob/step-1', data);
+    return this._HttpClient.post('https://scoob.cc/step-1', data);
   }
+  // upload(data: any): Observable<any> {
+  //   return this._HttpClient.post('/scoob/step-1', data);
+  // }
   pdfFile:any;
   setdata(data: any, file: File) {
     this.dataSubject.next(data);
@@ -63,10 +63,10 @@ export class UploadService {
   getSelectedBox() {
     return this.selectedBoxSubject.value;  
   }
-  //  reviewOmr(data:any): Observable<any> {
-  //   return this._HttpClient.post('https://scoob.cc/final', data);
-  // }
-  reviewOmr(data: any): Observable<any> {
-    return this._HttpClient.post(`/scoob/final`, data);
+   reviewOmr(data:any): Observable<any> {
+    return this._HttpClient.post('https://scoob.cc/final', data);
   }
+  // reviewOmr(data: any): Observable<any> {
+  //   return this._HttpClient.post(`/scoob/final`, data);
+  // }
 }
